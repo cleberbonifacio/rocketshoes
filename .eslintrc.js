f70a20de20cb7114f6c5cb34b0bab8d1938a570c
cleberbonifacio <bonifaciocleber@gmail.com>
2019-09-16 22:54:53 -0300

    cart

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "import/prefer-default-export": "off"
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-console": ["error", { allow: ["tron"]}]
   }
 };
