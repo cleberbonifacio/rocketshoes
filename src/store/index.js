e3a63cec1f30598fa14bd0a467b3a72d9ba068ed
Cleber Bonifacio <bonifaciocleber@gmail.com>
2020-05-27 09:04:02 -0300

    new store

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { createStore } from 'redux';
+import reducer from './modules/cart/reducer';
 
-import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/rootSaga';
-
-const sagaMonitor =
-  process.env.NODE_ENV === 'development'
-    ? console.tron.createSagaMonitor()
-    : null;
-
-const sagaMiddleware = createSagaMiddleware({
-  sagaMonitor,
-});
-
-const enhancer =
-  process.env.NODE_ENV === 'development'
-    ? compose(
-        console.tron.createEnhancer(),
-        applyMiddleware(sagaMiddleware)
-      )
-    : applyMiddleware(sagaMiddleware);
-
-const store = createStore(rootReducer, enhancer);
-
-sagaMiddleware.run(rootSaga);
+const store = createStore(reducer);
 
 export default store;
